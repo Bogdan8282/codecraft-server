d0ae6b9ab2043a592fb9d4876d3276b043d06d46
Богдан Матрофайло <144223853+Bogdan8282@users.noreply.github.com>
2026-05-09 16:06:20 +0300

    Added preview image to every post

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -3,6 +3,7 @@ import mongoose, { Schema, Document } from 'mongoose';
 export interface IPost extends Document {
   title: string;
   content: string;
+  imageURL: string;
   createdAt: Date;
   authorId: string;
   likes: string[];
@@ -12,6 +13,13 @@ export interface IPost extends Document {
 const PostSchema: Schema = new Schema({
   title: { type: String, required: true, trim: true },
   content: { type: String, required: true },
+  imageURL: {
+    type: String,
+    default: () => {
+      const randomSeed = Math.random().toString(36).substring(7);
+      return `https://api.dicebear.com/9.x/shapes/svg?seed=${randomSeed}`;
+    }
+  },
   createdAt: { type: Date, default: Date.now },
   authorId: { type: String, required: true },
   likes: { type: [String], default: [] },
